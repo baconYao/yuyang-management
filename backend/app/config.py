ae437aec5bcbe713a7a02b9aeec3dba8f63b2332
baconyao <bacon735392@gmail.com>
2025-11-19 11:36:33 -0500

    feat: add pg database for customer

---
+++ backend/app/config.py
@@ -0,0 +1,29 @@
+from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+_base_config = SettingsConfigDict(
+    env_file="./.env",
+    env_ignore_empty=True,
+    extra="ignore",
+)
+
+
+class DatabaseSettings(BaseSettings):
+    POSTGRES_SERVER: str = Field(default="localhost")
+    POSTGRES_PORT: int = Field(default=5432)
+    POSTGRES_USER: str = Field(default="postgres")
+    POSTGRES_PASSWORD: str = Field(default="postgres")
+    POSTGRES_DB: str = Field(default="test_db")
+
+    model_config = _base_config
+
+    @property
+    def POSTGRES_URL(self):
+        return (
+            f"postgresql+asyncpg://{self.POSTGRES_USER}:"
+            f"{self.POSTGRES_PASSWORD}@{self.POSTGRES_SERVER}:"
+            f"{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+        )
+
+
+db_settings = DatabaseSettings()
