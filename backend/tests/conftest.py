9f43fb5ed88880e6159d0fee39d780f13074ca29
baconyao <bacon735392@gmail.com>
2025-12-04 10:17:10 -0500

    fix: turn test cases into async mode

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,12 +1,17 @@
-import pytest
-from fastapi.testclient import TestClient
+import pytest_asyncio
+from httpx import ASGITransport, AsyncClient
 
 from app.main import app
 
 
-@pytest.fixture
-def client():
+@pytest_asyncio.fixture(scope="session")
+async def client():
     """
     創建 FastAPI 測試客戶端
     """
-    return TestClient(app)
+    # return TestClient(app)
+    async with AsyncClient(
+        transport=ASGITransport(app=app),
+        base_url="http://testserver",
+    ) as test_client:
+        yield test_client
